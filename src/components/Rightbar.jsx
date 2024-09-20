16fd7b7adfa1204df9cebac2ee914732592d4f9a
Mayorkun27 <156526127+Mayorkun27@users.noreply.github.com>
2024-09-20 15:21:45 +0100

    Updated the rightbar, designed the navbar, moving on to the rightbar

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -4,9 +4,9 @@ const RightBar = () => {
 
     return (
 
-        <>
+        <div className="flex justify-between bg-black text-accent-white w-1/5 h-[90vh] border-l border-neutral-600 p-3">
             <h3>Rightbar</h3>
-        </>
+        </div>
 
     )
 

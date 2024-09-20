61830177971ad52dca645adb3df27c1b6509a56e
Mayorkun27 <156526127+Mayorkun27@users.noreply.github.com>
2024-09-20 15:21:45 +0100

    Updated the rightbar, designed the navbar, moving on to the rightbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import img2 from "../assets/images/img2.png"
+import img1 from "../assets/images/img1.jpeg"
+import { CiBellOn } from "react-icons/ci";
 
 const NavBar = () => {
   return (
-    <div className='h-[8vh]'>NavBar</div>
+    <div className='h-[10vh] flex py-3 px-10 bg-black justify-between border-b border-neutral-600'>
+      <h3 className='font-extrabold text-accent-white text-2xl'>myTask</h3>
+      <div className="flex gap-3 items-center">
+        <CiBellOn className='text-accent-white text-2xl' />
+        <button className="btn bg-pry text-accent-black font-semibold rounded-md flex items-center justify-between border-2 border-accent-white gap-2 h-[40px] px-2 w-[150px]">
+          <span className='truncate w-10/12 text-sm'>Oluwamayokun</span>
+          <img src={img1} alt="..." className="w-[32px] h-[32px] rounded-full object-cover" />
+        </button>
+      </div>
+    </div>
   )
 }
 

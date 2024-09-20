c4b8f77307a89a5017d212bcf2c5c8954282aebc
Mayorkun27 <156526127+Mayorkun27@users.noreply.github.com>
2024-09-20 12:28:11 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/home/HomePage'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
+import TaskPage from './pages/dashboard/user/Taskpage'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path='/user' element={<UserDashboard/>}/>  
+        <Route path='/task' element={<TaskPage/>} />
       </Routes>
       <Footer/>
     </Router>
---
+++ src/components/Leftbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LeftBar = () => {
+
+    return (
+
+        <>
+            <h3>Leftbar</h3>
+        </>
+
+    )
+
+}
+
+export default LeftBar

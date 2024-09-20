7368f36f0dae1755debea57f51fbdb0c5e9646b8
codify01 <amoleuthman@gmail.com>
2024-09-20 17:27:56 +0100

    oh lord

--- src/components/Tasklist.jsx
+++ src/components/Tasklist.jsx
@@ -5,15 +5,9 @@ const Onetasklist = ({ href, title }) => {
     return(
 
         <>
-<<<<<<< HEAD
             <li className="font-medium text-base ms-2 opacity-50 rounded-md shadow hover:shadow-accent-black py-3 px-2 hover:opacity-100 hover:bg-pry hover:text-accent-white transition-all duration-300">
                 <a className="" href={href}>{title}</a>
             </li>
-=======
-            <a className="font-light text-base ms-2 opacity-50 rounded-md shadow-lg hover:shadow-accent-black p-2 hover:opacity-100 hover:bg-pry" href={href}>
-                {title}
-            </a>
->>>>>>> 6fee6408da33ae6da3d681311d0c790f5068729a
         </>
 
     )

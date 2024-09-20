4960b4e1bf66ca35092ff3fdec615ac98a383f11
Mayorkun27 <156526127+Mayorkun27@users.noreply.github.com>
2024-09-20 17:03:07 +0100

    Progress
    Finished with the right sidebar, Moving on to something else

--- src/components/Tasklist.jsx
+++ src/components/Tasklist.jsx
@@ -5,9 +5,9 @@ const Onetasklist = ({ href, title }) => {
     return(
 
         <>
-            <li className="font-light text-base ms-2 opacity-50 rounded-md shadow-lg hover:shadow-accent-black p-2 hover:opacity-100 hover:bg-pry">
-                <a className="" href={href}>{title}</a>
-            </li>
+            <a className="font-light text-base ms-2 opacity-50 rounded-md shadow-lg hover:shadow-accent-black p-2 hover:opacity-100 hover:bg-pry" href={href}>
+                {title}
+            </a>
         </>
 
     )

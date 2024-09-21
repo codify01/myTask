cf04cd8070d2e16e9a4c211e6f389d41ff0a219f
Mayorkun27 <156526127+Mayorkun27@users.noreply.github.com>
2024-09-21 11:13:29 +0100

    updated

--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React from 'react';
+import TaskDetailsCard from './Cards/TaskDetailsCard';
+import TaskCardOne from './Cards/TaskCardOne';
+import CarouselOne from './Carousels/CarouselOne';
+
+import Createtask from '../pages/dashboard/admin/create';
+import Teamembers from '../pages/dashboard/user/Team';
 
 const TaskDetails = () => {
-  return (
-    <div>
-        <div className=''>
-            <h1>Task Details</h1>
-            <hr className='border-2'/>
-        </div>
-    </div>
-  )
-}
+  const list = [
+    1,,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,11,1,1,1
+  ]
+	return (
+		<div className="sContainer mt-3 space-y-4 h-[93vh] overflow-y-auto pb-24">
+			<Teamembers/>
+			<div className="flex items-center justify-center">
+				<h1 className="basis-1/3 text-4xl font-bold">Task Details</h1>
+				<hr className="hidden md:block border border-accent-black dark:border-accent-white rounded-full basis-2/3" />
+			</div>
+			<CarouselOne/>
+			{
+				list.map(()=>(
+				<TaskDetailsCard />
+				))
+			}
+			<Createtask/>
+		</div>
+	);
+};
 
-export default TaskDetails
+export default TaskDetails;
---
+++ src/components/Teamtasklist.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Teamtasklist = ({task}) => {
+
+    return (
+
+        <>
+            <li className="p-1 truncate border-b border-neutral-400">{task}</li>
+        </>
+
+    )
+
+}
+
+export default Teamtasklist;

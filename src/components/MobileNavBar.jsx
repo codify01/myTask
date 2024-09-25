debb9120f996f257a29b568087791a7a3b10a0fd
codify01 <amoleuthman@gmail.com>
2024-09-25 20:45:35 +0100

    soting

--- src/components/MobileNavBar.jsx
+++ src/components/MobileNavBar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from "react";
 import { MdSpaceDashboard, MdGroups2, MdCardMembership, MdAssignment, MdPendingActions } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
@@ -7,20 +6,10 @@ import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 import img1 from "../assets/images/img1.jpeg";
 
 
-=======
-import React from "react";
-import { MdSpaceDashboard, MdGroups2, MdCardMembership, MdAssignment, MdPendingActions } from "react-icons/md";
-import { FaUser } from "react-icons/fa";
-import { IoCreate, IoLogOut } from "react-icons/io5";
-import { Link, NavLink, useLocation } from "react-router-dom";
-import img1 from "../assets/images/img1.jpeg";
-
->>>>>>> acaf3b47e49bafb787bf430329849b2cf9a974bc
 const eachTask = [
   {
     href: "/user/dashboard",
     title: "Dashboard",
-<<<<<<< HEAD
     icon: <MdSpaceDashboard className="size-6" />,
     roles: ["admin", "user"] 
   },
@@ -111,46 +100,6 @@ const MobileLeftBar = ({ isOpen, toggleSidebar }) => {
     localStorage.removeItem('pendingTask')
     navigate('/login')
 };
-=======
-    icon: <MdSpaceDashboard className="size-6" />
-},
-{
-    href: "/admin/add-user",
-    title: "Create user",
-    icon: <IoCreate className="size-6" />
-},
-{
-    href: "/admin/assign-task",
-    title: "Assign task",
-    icon: <MdAssignment className="size-6" />
-},
-{
-    href: "/team",
-    title: "Team Member",
-    icon: <MdCardMembership className="size-6" />
-},
-{
-    href: "/profile",
-    title: "Profile",
-    icon: <FaUser className="size-6" />
-},
-{
-    href: "/groups",
-    title: "Groups",
-    icon: <MdGroups2 className="size-6" />
-},
-{
-    href: "/admin/pending-task",
-    title: "Pending task",
-    icon: <MdPendingActions className="size-6" />
-},
-]
-
-const MobileLeftBar = ({ isOpen, toggleSidebar }) => {
-  const ignoredPages = ['/', '/login', '/about', '*'];
-  const location = useLocation();
-  const isHere = ignoredPages.includes(location.pathname);
->>>>>>> acaf3b47e49bafb787bf430329849b2cf9a974bc
 
   return (
     <>
@@ -159,7 +108,6 @@ const MobileLeftBar = ({ isOpen, toggleSidebar }) => {
           isOpen ? 'translate-x-0' : '-translate-x-full'
         }`}
       >
-<<<<<<< HEAD
         {!isHere && sessionData && (
           <div className="space-y-5 p-5">
             <div className="flex flex-col items-center w-full text-center">
@@ -172,17 +120,6 @@ const MobileLeftBar = ({ isOpen, toggleSidebar }) => {
 
             <ul className="space-y-4">
               {filteredTasks.map(({ href, title, icon }, index) => (
-=======
-        {!isHere && (
-          <div className="space-y-5 p-5">
-            <div className="flex flex-col items-center w-full text-center">
-              <img src={img1} alt="User profile" className="w-20 h-20 rounded-full object-cover" />
-              <h3 className="font-bold text-lg truncate w-full">Adeleke Oluwamayokun</h3>
-              <small className="truncate w-full text-xs">adelekeoluwamayokun27@gmail.com</small>
-            </div>
-            <ul className="space-y-4">
-              {eachTask.map(({ href, title, icon }, index) => (
->>>>>>> acaf3b47e49bafb787bf430329849b2cf9a974bc
                 <NavLink 
                   to={href} 
                   key={index} 
@@ -194,16 +131,11 @@ const MobileLeftBar = ({ isOpen, toggleSidebar }) => {
                 >
                   <div onClick={toggleSidebar} className="flex items-center gap-3 mb-3 font-medium text-base ms-2 rounded-md py-4 px-2 hover:opacity-100">
                     {icon}
-<<<<<<< HEAD
                     <li>{title}</li>
-=======
-                    <li className="">{title}</li>
->>>>>>> acaf3b47e49bafb787bf430329849b2cf9a974bc
                   </div>
                 </NavLink>
               ))}
             </ul>
-<<<<<<< HEAD
             <button
                     onClick={handleLogout}
                     className="absolute bg-accent-black border-t border-neutral-700 ps-2 w-full left-0 bottom-0 h-[40px] flex gap-2 items-center font-semibold text-neutral-500 hover:text-neutral-500/60"
@@ -211,26 +143,12 @@ const MobileLeftBar = ({ isOpen, toggleSidebar }) => {
                     <span>Log Out</span>
                     <IoLogOut className="size-6" />
                 </button>
-=======
-            <Link 
-              to="/" 
-              className="absolute bottom-0 left-0 w-full flex items-center justify-center text-neutral-500 font-medium p-2 border-t border-neutral-700"
-              onClick={toggleSidebar} 
-            >
-              Log Out
-              <IoLogOut className="ml-2" />
-            </Link>
->>>>>>> acaf3b47e49bafb787bf430329849b2cf9a974bc
           </div>
         )}
       </div>
       {isOpen && (
         <div
-<<<<<<< HEAD
           className="fixed inset-0 bg-black bg-opacity-50 z-30"
-=======
-          className="fixed inset-0 bg-black bg-opacity-50"
->>>>>>> acaf3b47e49bafb787bf430329849b2cf9a974bc
           onClick={toggleSidebar}
         />
       )}
